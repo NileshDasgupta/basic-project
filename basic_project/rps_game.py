5ca2fba33486dbae352b34d1d7ef8ceab803fee2
NileshDasgupta <nileshdasgupta07@gmail.com>
2026-08-28 14:18:22 +0530

    Added new Python project and updated files

--- basic_project/rps_game.py
+++ basic_project/rps_game.py
@@ -5,30 +5,45 @@ choose = ("r","p","s")
 
 computer_choose = random.choice(choose)
 
-while True:
-    user_input =input("choose rock,paper,scisor(r/p/s): ").lower()
-    if user_input not in choose:
-        print("invalid choice")
-        continue
+def get_the_user_input():
+    while True:
+        user_input =input("choose rock,paper,scisor(r/p/s): ").lower()
+        if user_input not in choose:
+            print("invalid choice")
+            continue
+        else:
+            break
+    return user_input
 
+def display_of_choice(user_input):
     print(f"you choosed {emojis[user_input]}")
     print(f"computer choosed {emojis[computer_choose]}")
-
-    if user_input == computer_choose:
-        print("tie")
-
-    elif (
+ 
+def game_logic(user_input):
+      if user_input == computer_choose:
+            print("tie")
+    
+      elif (
         (user_input == "r" and computer_choose == "s") or
         (user_input == "p" and computer_choose == "r") or
         (user_input == "s" and computer_choose == "p")):
         print("you win")
+    
+      else:
+         print("you lost")
 
-    else:
-        print("you lost")
+def play_game():
+    while True:
+        user_input = get_the_user_input()
+        display_of_choice(user_input)
+        game_logic(user_input)
+        continue_play = input("do you want to continue(y/n): ").lower()
+        if continue_play == "n":
+            break
 
-    continue_play = input("do you want to continue(y/n): ").lower()
-    if continue_play == "n":
-        break
+play_game()
+
+
 
 
     
